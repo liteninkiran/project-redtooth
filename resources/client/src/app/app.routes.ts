df5fc33ef99a98a1e0e431422470eaf8bb81ab3d
Kiran Anand <liteninkiran@hotmail.com>
2025-10-01 11:45:21 +0100

    Add venue component

--- resources/client/src/app/app.routes.ts
+++ resources/client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Venue } from './pages/venue/venue';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'venue',
+        pathMatch: 'full',
+    },
+    {
+        path: 'venue',
+        component: Venue,
+    },
+];
