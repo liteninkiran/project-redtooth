d009c916b4b305be2817bbf50b87d89506619d15
Kiran Anand <liteninkiran@hotmail.com>
2025-11-29 16:29:23 +0000

    Add ag-grid

--- resources/client/src/app/app.config.ts
+++ resources/client/src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- resources/client/src/app/pages/venue/venue.ts
+++ resources/client/src/app/pages/venue/venue.ts
@@ -1,14 +1,42 @@
 import { Component, inject } from '@angular/core';
 import { VenueService } from '../../services/venue.service';
 import { CommonModule } from '@angular/common';
-
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
 @Component({
     selector: 'app-venue',
-    imports: [CommonModule],
+    imports: [CommonModule, AgGridAngular],
     templateUrl: './venue.html',
     styleUrl: './venue.scss',
 })
 export class Venue {
     private venueService = inject(VenueService);
     public venues$ = this.venueService.getVenues();
+
+    // Column Definitions: Defines the columns to be displayed.
+    colDefs: ColDef[] = [
+        { field: 'id' },
+        { field: 'venueid' },
+        { field: 'venuename' },
+        { field: 'lat' },
+        { field: 'longi' },
+        { field: 'active' },
+        { field: 'venueaddress1' },
+        { field: 'venueaddress2' },
+        { field: 'venuetown' },
+        { field: 'venuecounty' },
+        { field: 'venuepostcode' },
+        { field: 'gamenight' },
+        { field: 'venueimage' },
+        { field: 'landlordfn' },
+        { field: 'landlordsn' },
+        { field: 'venuetelno' },
+        { field: 'landlordtitle' },
+        { field: 'imageapproval' },
+        { field: 'distance_miles' },
+        { field: 'venue_status_id' },
+        { field: 'map_html' },
+        { field: 'created_at' },
+        { field: 'updated_at' },
+    ];
 }
